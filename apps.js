332b7f9ba6c2713dc6149f245afd05b70bed2982
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-26 12:22:44 +0300

    function

---
+++ apps.js
@@ -0,0 +1,30 @@
+// function = is block of code that perform specific task
+
+// function declaration
+
+// function greet (){
+//     console.log('hello world')
+// }
+// greet();
+
+``
+
+function sayHello(name){
+    console.log(`hello ${name}`);
+}
+sayHello("Ahmed");
+sayHello("yussuf");
+
+function add(x, y){
+    return x + y;
+   
+}
+ console.log(add(20, 10))
+ 
+//  function experation
+
+const greeting = function (user){
+    console.log(`hello ${user}`)
+};
+greeting('how are you')
+
